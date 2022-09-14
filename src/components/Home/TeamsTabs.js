fdc5b00950f78d2b5dbfa433e07cc66bd5ac9d9b
Ganesh <ganesh.shetty@codewave.com>
2022-09-14 11:35:47 +0530

    todats changes

--- src/components/Home/TeamsTabs.js
+++ src/components/Home/TeamsTabs.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {
-  Tab, Tabs, TabList, TabPanel
-} from 'react-tabs';
+// import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 // import Tabicondiv from '../../assets/icon11.png';
 import Tabimg1 from '../../assets/tabimg1.png';
 import Tabimg2 from '../../assets/tabimg2.jpg';
-// import { Tabicon1 } from '../../assets';
+import { Tabicon1 } from '../../assets';
+import tabicon3 from '../../assets/tabicon3.png';
+// import icon3 from '../../assets/icon3.png';
+import icon4 from '../../assets/icon4.png';
+import icon5 from '../../assets/icon5.png';
+import icon6 from '../../assets/icon6.png';
+import icon7 from '../../assets/icon7.png';
 
 const TeamsTabs = () => (
   <div className='container'>
@@ -20,7 +24,7 @@ const TeamsTabs = () => (
       </p>
     </div>
     <div className='row'>
-      <Tabs>
+      {/* <Tabs>
         <TabList>
           <Tab>
             <p className='sidetabtext'>
@@ -144,17 +148,93 @@ const TeamsTabs = () => (
             </div>
           </div>
         </TabPanel>
-      </Tabs>
-    </div>
-    {/* <div className='col-sm-5'>
-      <div className='row'>
-        <div className='col-sm-3'>
-          <img alt='Tabicon' src={Tabicon1.default} />
+      </Tabs> */}
+      <div className='col-sm-5 pad-0'>
+        <div className='row row-pad rowflex'>
+          <div className='col-sm-2 pad-0'>
+            <img alt='Tabicon' src={Tabicon1.default} />
+          </div>
+          <div className='col-sm-10'>
+            <p className='teamtext'>
+              Self organize around a
+              <br />
+              shared purpose.
+            </p>
+          </div>
         </div>
-        <div className='col-sm-9'>sdffdg</div>
+        <div className='row row-pad rowflex'>
+          <div className='col-sm-2 pad-0'>
+            <img alt='Tabicon' src={tabicon3} />
+          </div>
+          <div className='col-sm-10'>
+            <p className='teamtext'>
+              Set the big picture &
+              <br />
+              goals, jointly
+            </p>
+          </div>
+        </div>
+        <div className='row row-pad rowflex'>
+          <div className='col-sm-2 pad-0'>
+            <img alt='Tabicon' src={icon4} />
+          </div>
+          <div className='col-sm-10'>
+            <p className='teamtext'>
+              Make decisions, leaving no-
+              <br />
+              one behind
+            </p>
+          </div>
+        </div>
+        <div className='row row-pad rowflex'>
+          <div className='col-sm-2 pad-0'>
+            <img alt='Tabicon' src={icon5} />
+          </div>
+          <div className='col-sm-10'>
+            <p className='teamtext'>
+              Jam regularly to reflect
+              <br />
+              and learn
+            </p>
+          </div>
+        </div>
+        <div className='row row-pad rowflex'>
+          <div className='col-sm-2 pad-0'>
+            <img alt='Tabicon' src={icon6} />
+          </div>
+          <div className='col-sm-10'>
+            <p className='teamtext'>
+              Measure what matters,
+              <br />
+              very often
+            </p>
+          </div>
+        </div>
+        <div className='row row-pad rowflex'>
+          <div className='col-sm-2 pad-0'>
+            <img alt='Tabicon' src={icon7} />
+          </div>
+          <div className='col-sm-10'>
+            <p className='teamtext'>
+              Celebrate the outcomes
+              <br />
+              and impact
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='col-sm-7 teamimgdiv'>
+        <div className='row'>
+          <div className='col-sm-6 panelimg'>
+            <img src={Tabimg1} alt='sfsd' />
+          </div>
+
+          <div className='col-sm-6 panelseconddiv'>
+            <img src={Tabimg2} alt='sfsd' />
+          </div>
+        </div>
       </div>
     </div>
-    <div className='col-sm-7'>sdfdeffdsfsdsf</div> */}
   </div>
 );
 
